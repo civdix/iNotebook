ac566cc0942e2e4109d6f3db34afcd0a3da60e2e
Shivam Dixit <archanavrindavanvlog@gmail.com>
2024-11-03 23:47:26 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/iNotebook";
+const mongoURI = process.env.MONGO_DB_URI;
 
 const connectToMongo = async () => {
   try {
