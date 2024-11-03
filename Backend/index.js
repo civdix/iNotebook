117f2385dce528c74b49dd07796dec138aff3f10
Shivam Dixit <archanavrindavanvlog@gmail.com>
2024-11-04 03:31:33 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require('cors');
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
-  next();
-});
+app.use(cors({
+  origin:"https://i-notebook-weld.vercel.app"),
+  methods:["GET","POST","DELETE","PUSH"]}))
 app.use(express.json());
 app.use("/api/auth/", require("./routes/auth"));
 app.use("/api/notes/", require("./routes/notes"));
