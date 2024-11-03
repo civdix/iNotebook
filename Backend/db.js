6c7c7afbc3bca5f719c56d3f03965b33e8c3798a
Shivam Dixit <archanavrindavanvlog@gmail.com>
2024-11-04 05:03:29 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const mongoURI ="mongodb+srv://shivam:124124@inotebook.iadnz.mongodb.net/?retryWrites=true&w=majority&appName=iNotebook";
 
 const connectToMongo = async () => {
+  console.log("In The Connecttomongo")
   try {
     await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB successfully!");
