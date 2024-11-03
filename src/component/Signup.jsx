a7e5766ab5accaa1830ec72e539f60f23f1d4bbf
Shivam Dixit <archanavrindavanvlog@gmail.com>
2024-11-04 01:01:41 +0530

    Update Signup.jsx

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("i-notebook-api-three.vercel.app/api/auth/signUp", {
+      const response = await fetch("http://i-notebook-api-three.vercel.app/api/auth/signUp", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
