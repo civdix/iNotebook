5bec238489e798cb3122d1e6711715442e8c793d
Shivam Dixit <archanavrindavanvlog@gmail.com>
2024-11-04 01:48:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require('cors');
-// Allow requests from http://localhost:3000
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+  next();
+});
 app.use(express.json());
 app.use("/api/auth/", require("./routes/auth"));
 app.use("/api/notes/", require("./routes/notes"));
