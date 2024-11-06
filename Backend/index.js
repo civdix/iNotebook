5b52ff837e9a47e2611fdc31de245e39f517b4d6
Shivam Dixit <iayushtiwari3@gmail.com>
2024-11-06 14:50:13 +0530

    Refactorin 04

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,8 @@ app.use(
 app.use(express.json());
 app.use("/api/auth/", require("./routes/auth"));
 app.use("/api/notes/", require("./routes/notes"));
-app.get("/", (req, res) => {
+
+app.get("", (req, res) => {
   res.send("Hello World!");
 });
 
