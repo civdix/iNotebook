1c56c3359fb814cde23ed16fc8997b5b25550068
Shivam Dixit <iayushtiwari3@gmail.com>
2024-11-06 09:27:03 +0530

    Refacotring

--- Backend/db.js
+++ Backend/db.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-require("dotenv").config();
-const mongoURI = process.env.MONGO_DB_URI;
 
-const connectToMongo = async () => {
-=======
+// const mongoURI = process.env.MONGO_DB_URI;
 const mongoURI ="mongodb+srv://shivam:124124@inotebook.iadnz.mongodb.net/?retryWrites=true&w=majority&appName=iNotebook";
 
+
+  
 const connectToMongo = async () => {
   console.log("In The Connecttomongo")
->>>>>>> origin/main
+
   try {
     await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB successfully!");
